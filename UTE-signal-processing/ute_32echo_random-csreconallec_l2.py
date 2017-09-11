bdd56574e7c60863f64a76de8a4308f5a07520f0
Uday Suresh <Uday@Udays-MacBook-Pro.local>
2017-09-10 18:30:32 -0700

    wip, no longer using scipy atm

--- UTE-signal-processing/ute_32echo_random-csreconallec_l2.py
+++ UTE-signal-processing/ute_32echo_random-csreconallec_l2.py
@@ -2,7 +2,31 @@
 
 import scipy.io as sio
 
-sio.whosmat('ute_32echo_random-csreconallec_l2_r0p01.mat')
-mat_contents = sio.loadmat('ute_32echo_random-csreconallec_l2_r0p01.mat')
+# sio.whosmat('ute_32echo_random-csreconallec_l2_r0p01.mat')
+# mat_contents = sio.loadmat('ute_32echo_random-csreconallec_l2_r0p01.mat')
 # print(mat_contents)
 ## for jupyter in case I wanna make sure
+
+### ^^^ none of this works because matlab 7.3 files can't be opened with scipy
+
+import h5py as hp
+import numpy as np
+
+mat_contents = hp.File('ute_32echo_random-csreconallec_l2_r0p01.mat')
+
+#get the keys
+print(mat_contents.keys())
+
+#sizing the datasets
+print(mat_contents['TE'])
+print(mat_contents['imallplus'])
+
+
+
+# arrays ={}
+# for a, b in mat_contents.items():
+#         arrays[a] = np.array(b)
+
+
+
+#should give the variables within the file with these keys to access a dict
