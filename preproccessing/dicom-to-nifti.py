abe22affd05ead0f8c52d837d039da9a0e9dbe58
Uday Suresh <udaysuresh1@gmail.com>
2017-09-30 21:15:42 -0700

    basic converter for my MRI data

---
+++ preproccessing/dicom-to-nifti.py
@@ -0,0 +1,16 @@
+## quick & basic converter for my own MRI files to swap into nifti from dicoms
+## I deserve this for sitting in MRI scanner for an hour
+
+from nipype.interfaces.dcm2nii import Dcm2nii
+
+converter = Dcm2nii()
+
+# throw inputs here
+converter.inputs.source_names = ['NAME.dcm']
+
+# says this is a good idea online bc of size/compression
+converter.inputs.gzip_output = True
+converter.inputs.output_dir = '.'
+
+converter.cmdline
+'dcm2nii -a y -c y -b config.ini -v y -d y -e y -g y -i n -n y -o . -p y -x n -f n NAME.dcm'
