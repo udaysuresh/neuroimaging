3bba3a1fd3018d32d12ad3c7ae3d6634ba6eabc6
Uday Suresh <udaysuresh1@gmail.com>
2017-10-06 11:38:30 -0700

    first try

--- ute/T1_T2_GM_WM_segmentation.py/T2_GM/WM_segmentation.py
+++ ute/T1_T2_GM_WM_segmentation.py/T2_GM/WM_segmentation.py
@@ -4,8 +4,21 @@
 from nipype.interfaces.dcm2nii import Dcm2nii
 import nipype.interfaces.fsl as fsl
 import nipype.pipeline.engine as pe
-
-
-converter = Dcm2nii()
+import nipype.interfaces.io as nio
 
 converter.inputs.source_names = ['NAME.dcm']
+
+convert = pe.MapNode(interface=Dcm2nii(), name='conv', iterfield=['source_names'])
+
+fast = pe.MapNode(interface=fsl.FAST(), name='fast', iterfield=['in_files'])
+
+ds = pe.Node(interface=nio.DataSink(), name="ds", iterfield=['in_files'])
+
+ds.inputs.base_directory = '.'
+
+workflow = pe.Workflow(name='T1/T2_GM/WM_Segmentation')
+workflow.base_dir = '.'
+
+workflow.connect([(conv, fast, [('out_file', 'in_files')]), (fast, ds,[('mixeltype', 'in_files')])])
+
+workflow.run()
