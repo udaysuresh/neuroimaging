f38d4499c31c82aa8834b58873d667637c68f384
Uday Suresh <udaysuresh1@gmail.com>
2017-10-07 17:01:58 -0700

    need help adding networkx

--- preproccessing/dicom-to-nifti.py
+++ preproccessing/dicom-to-nifti.py
@@ -2,15 +2,16 @@
 ## I deserve this for sitting in MRI scanner for an hour
 
 from nipype.interfaces.dcm2nii import Dcm2nii
+import networkx
 
 converter = Dcm2nii()
 
 # throw inputs here
-converter.inputs.source_names = ['/Users/Uday/Documents/personal_brain_data/2017-09-29_3T-volunteer/E5466/1/E5466S1I1.DCM']
+converter.inputs.source_names = ['./../ute/E5466S1I1.DCM']
 
 # says this is a good idea online bc of size/compression
 converter.inputs.gzip_output = True
-converter.inputs.output_dir = '/Users/Uday/Documents/personal_brain_data/2017-09-29_3T-volunteer/E5466/'
+converter.inputs.output_dir = '.'
 
 converter.cmdline
-'dcm2nii -a y -c y -b config.ini -v y -d y -e y -g y -i n -n y -o . -p y -x n -f n E5466S1I1.DCM'
+'dcm2nii -a y -c y -b config.ini -v y -d y -e y -g y -i n -n y -o . -p y -x n -f n /../ute/E5466S1I1.DCM'
