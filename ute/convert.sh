e510effddadf6679761745aa0a1d7d888ab9ece6
Uday Suresh <udaysuresh1@gmail.com>
2017-10-10 17:01:58 -0700

    more basic -> trying an alternative to dcm2nitfi

--- ute/convert.sh
+++ ute/convert.sh
@@ -2,10 +2,6 @@ BASE_DIR=/raw_dicom    # location of raw data folder
 T1_FOLDER=t1w_3d_MPRAGE            # dicom folder containing anatomical scan
 DATA_DIR=/data        # location of output folder
 
-
-
-
-for id in $(seq -w 1 10)
 do
-    mri_convert $BASE_DIR/sub0$id/$T1_FOLDER/00001.dcm    $DATA_DIR/sub0$id/struct.nii.gz
+    mri_convert $BASE_DIR/$T1_FOLDER/ name .dcm    $DATA_DIR/sub0$id/struct.nii.gz
 done
