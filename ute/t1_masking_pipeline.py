40b34d633e56b7f8a7e33cde04fc15b47e8abe42
Uday Suresh <udaysuresh1@gmail.com>
2017-10-13 13:57:46 -0700

    need UTE for registration

--- ute/t1_masking_pipeline.py
+++ ute/t1_masking_pipeline.py
@@ -3,19 +3,29 @@
 
 import os
 import nipype.interfaces.fsl as fsl
+import nipype.pipeline.engine as pe
+
 
 T1 = "/my/directory/filename.dcm"
 directory = os.path.dirname(file_path)
 
 # TODO: convert dcm to nifti here
 
-## preprocessing params
+## T1 masking
 
 bet = pe.MapNode(interface=fsl.BET(), name = 'bet')
+
+## preprocessing params
 
 bet.inputs.vertical_gradient = 0.45
 bet.inputs.mask = 0
 bet.inputs.robust = True
 bet.inputs.frac = 0.5 #default <- change this to adjust mask
 
-## T1 masking
+## T1 segmentation
+
+fast = pe.MapNode(interface=fsl.FAST(), name='fast')
+
+# TODO: register UTE magnitude to T1
+
+# TODO: apply to frequency map or chemical shift map
