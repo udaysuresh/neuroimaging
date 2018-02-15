d5cff2aaf5bfaf56db11929485f341e9621c9595
Uday Suresh <udaysuresh1@gmail.com>
2018-02-15 14:42:04 -0800

    dev prog

--- ute/skull_strip.py
+++ ute/skull_strip.py
@@ -1,29 +1,28 @@
 
-from nipype.interfaces.dcm2nii import Dcm2nii
+import nipype.interfaces.dcm2nii as dcm2nii
 import nipype.interfaces.fsl as fsl
 import nipype.pipeline.engine as pe
 import nipype.interfaces.io as nio
 
-converter = Dcm2nii()
-converter.source_dir = '2'
-
-
+dcm2nii = pe.Node(interface=dcm2nii(), name = 'dcm2nii')
+dcm2nii.source_dir = '3'
 
 # skull stripping
 
 bet = pe.Node(interface=fsl.BET(), name = 'bet')
-bet.inputs.in_file = '20170929_dti_FA.nii.gz'
 
 # segmentation
-fast = pe.Node(interface=fsl.FAST(), name='fast')
+# fast = pe.Node(interface=fsl.FAST(), name='fast')
 
-ds = pe.Node(interface=nio.DataSink(), name="ds")
+# ds = pe.Node(interface=nio.DataSink(), name="ds")
 
 ds.inputs.base_directory = '.'
 
-workflow = pe.Workflow(name='bet_fast')
+workflow = pe.Workflow(name='conv_bet')
 workflow.base_dir = './output'
 
-workflow.connect([(bet, fast, [('out_file', 'in_files')]), (fast, ds, [('mixeltype', 'in_files')])])
+workflow.connect([(dcm2nii, bet, [('converted_files', 'in_file')])])
+# workflow.connect([(dcm2nii, bet, [('converted_files', 'in_file')]), (bet, fast, [('out_file', 'in_files')])])
+
 
 workflow.run()
