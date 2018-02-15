0229e02de2959434202ca70d6fc81aba874de57c
Uday Suresh <udaysuresh1@gmail.com>
2018-02-15 14:17:59 -0800

    dcm -> nii

--- ute/skull_strip.py
+++ ute/skull_strip.py
@@ -4,7 +4,13 @@ import nipype.interfaces.fsl as fsl
 import nipype.pipeline.engine as pe
 import nipype.interfaces.io as nio
 
+converter = Dcm2nii()
+converter.source_dir = '2'
+
+
+
 # skull stripping
+
 bet = pe.Node(interface=fsl.BET(), name = 'bet')
 bet.inputs.in_file = '20170929_dti_FA.nii.gz'
 
@@ -15,7 +21,7 @@ ds = pe.Node(interface=nio.DataSink(), name="ds")
 
 ds.inputs.base_directory = '.'
 
-workflow = pe.Workflow(name='T1_T2_Segmentation')
+workflow = pe.Workflow(name='bet_fast')
 workflow.base_dir = './output'
 
 workflow.connect([(bet, fast, [('out_file', 'in_files')]), (fast, ds, [('mixeltype', 'in_files')])])
